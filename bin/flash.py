7126bf77e381724188e71fbf1a168f8417fc0267
Bernhard Boser <boser@berkeley.edu>
2017-11-22 13:50:31 -0800

    fixed flash.py

--- bin/flash.py
+++ bin/flash.py
@@ -2,7 +2,7 @@
 
 import os
 
-iot_dir = os.getenv('IoT49', os.path.join(os.path.expanduser('~'), 'IoT49')
+iot_dir = os.getenv('IoT49', os.path.join(os.path.expanduser('~'), 'IoT49'))
 fw_dir  = os.path.join(iot_dir, 'firmware/dist')
 port = os.getenv('RSHELL_PORT')
 
