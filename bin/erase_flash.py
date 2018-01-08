88500538b3fb58c71a59ccd2b01b89095a65760d
Bernhard Boser <boser@berkeley.edu>
2018-01-07 17:49:41 -0800

    reorganized

--- bin/erase_flash.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import os
-
-port = os.getenv('RSHELL_PORT')
-
-cmd = "esptool.py --port {} erase_flash".format(port)
-
-os.system(cmd)
--- esp32/mcu/lib/mpu9x50/__init__.py
+++
@@ -1,4 +0,0 @@
-# add folder to python path
-
-import sys
-sys.path.append(sys.modules[__name__].__path__)
